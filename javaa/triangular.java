a9af8bb6af230a27ad2d76fd366725d4b564d11b
Sandhya <144054124+sandhya-1234@users.noreply.github.com>
2024-02-15 07:14:58 +0530

    Create triangular.java

---
+++ javaa/triangular.java
@@ -0,0 +1,17 @@
+public class TriangularNumbers {
+    public static void main(String[] args) {
+        int rows = 5; 
+       
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces for alignment
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+           
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
